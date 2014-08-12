2f079f054ba3fd3f14a8d47e44bce2b0c9a7daca
Cadyyan <cadyyan@gmail.com>
2014-08-12 16:19:52 -0500

    Update setup.py to actually do stuff

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
-import setuptools
+from setuptools import setup, find_packages
 
-# TODO: write setup script
+setup(
+	name             = 'MCServer Tools',
+	version          = '1.0.0.0',
+	description      = 'Minecraft Server tools for vanilla and Forge modded servers.',
+	author           = 'Cadyyan',
+	url              = 'https://github.com/cadyyan/mcserver-tools',
+	packages         = find_packages(),
+	scripts          = ['bin/mcserver'],
+	install_requires = [
+		'daemon',
+		'tmuxp',
+	],
+)
+
